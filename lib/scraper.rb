38bafa4cbaffeec562f96e51f38326b6a0f43477
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-22 17:21:58 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,7 +24,7 @@ class Scraper
     student = {}
     links = []
     learnco.css("div.social-icon-container a").each do |link|
-      links << link
+      links << link.attribute("href").value
     end
     binding.pry
     #  link_text = learnco.css("div.social-icon-container a").attribute("href").value
