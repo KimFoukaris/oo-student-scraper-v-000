9ef3f6bed3a93c7a1b6b46807237c1b54459e457
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-22 16:31:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,47 +7,56 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = File.read(index_url)
     learnco = Nokogiri::HTML(html)
-    students_array = []
+    array = []
     learnco.css("div.student-card").each do |student|
-      students_array << {
-      :name => student.css("h4.student_name").text,
-      :location => student.css("p.student_location").text,
+      array << {
+      :name => student.css("h4.student-name").text,
+      :location => student.css("p.student-location").text,
       :profile_url => student.css("a").attribute("href").value
-        }
-    #binding.pry
+      }
     end
-    students_array
+    array
   end
+
+#ryan-johnson-card > a > div.card-text-container > h4
 
   def self.scrape_profile_page(profile_url)
     html = File.read(profile_url)
     learnco = Nokogiri::HTML(html)
+    student = {}
+    #learnco.css("div.social-icon-container a").each do |link|
+    #  link_text = learnco.css("div.social-icon-container a").attribute("href").value
+    #  case
+    #  when link_text.include?("twitter")
+    #    t_link = learnco.css("div.social-icon-container a").attribute("href").value
+    #  when link_text.include?("linkedin")
+    #    l_link = learnco.css("div.social-icon-container a").attribute("href").value
+    #  when link_text.include?("github")
+    #    g_link = learnco.css("div.social-icon-container a").attribute("href").value
+    #  else
+    #    b_link = learnco.css("div.social-icon-container a").attribute("href").value
+  #    end
+    #  binding.pry
+  #  end
+#binding.pry
+  #student = {
+  #    :profile_quote => learnco.css("div.vitals-text-container div").text,
+  #    :bio => learnco.css("div.description-holder p").text,
+  #    :twitter => t_link, #learnco.css("div.social-icon-container a")[0].attribute("href").value,
+  #    :linkedin => l_link, #learnco.css("div.social-icon-container a")[1].attribute("href").value,
+  #    :github => g_link, #learnco.css("div.social-icon-container a")[2].attribute("href").value,
+  #    :blog => b_link #learnco.css("div.social-icon-container a")[3].attribute("href").value
+  #  }
+
     student = {
       :profile_quote => learnco.css("div.vitals-text-container div").text,
-      :bio => learnco.css("div.description-holder p").text
+      :bio => learnco.css("div.description-holder p").text,
+      :twitter => learnco.css("div.social-icon-container a")[0].attribute("href").value,
+      :linkedin => learnco.css("div.social-icon-container a")[1].attribute("href").value,
+      :github => learnco.css("div.social-icon-container a")[2].attribute("href").value,
+      :blog => learnco.css("div.social-icon-container a")[3].attribute("href").value
     }
 
-
-  #
-      #if link.css("a").attribute("href").value.include? "twitter"
-    #   :twitter => learnco.css("a").attribute("href").value,
-      #elsif link.css("a").attribute("href").value.include? "linkedin"
-    #    :linkedin => learnco.css("a").attribute("href").value,
-      #elsif link.css("a").attribute("href").value.include? "github"
-    #    :github => learnco.css("a").attribute("href").value,
-      #else
-  #      :blog => learnco.css("a").attribute("href").value,
-
-
-  #  end
-    #:linkedin => learnco.css("div.social-icon-container a").attribute("href").value
-    #:github => learnco.css("div.social-icon-container a").attribute("href:contains("github")").value
-    #:blog => learnco.css("div.social-icon-container a").attribute("href").value
-  #
-#  }
-    #}
-    #binding.pry
-  #  end
   end
 
 end
